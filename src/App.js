fe7da23cc9e5d0cf2f4a22f9c118400f388a52f1
mateussousaa <mateussousa20@outlook.com>
2022-08-05 17:02:14 -0300

    starting the requirement 6

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <Switch>
+      <Route component={ Game } path="/game" />
       <Route component={ Login } path="/" />
     </Switch>
   );
