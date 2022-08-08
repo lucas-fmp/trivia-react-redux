9bf274fbf2fc157273f720b4c766e5263a03dae2
lucas-fmp <lucasfernandomacedo13@gmail.com>
2022-08-08 19:52:47 -0300

    requirement 08 - complete

--- src/components/QuestionBoard.js
+++ src/components/QuestionBoard.js
@@ -4,6 +4,24 @@ import React, { Component } from 'react';
 let incorrectIdx = 0;
 
 export default class QuestionBoard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      seconds: 30,
+      randomAnswers: [],
+      buttonState: false,
+    };
+  }
+
+  componentDidMount = () => {
+    const { questionInfo } = this.props;
+    const { correct_answer: correctAnswer } = questionInfo;
+    const answers = [...questionInfo.incorrect_answers, correctAnswer];
+    const randomAnswers = this.randomizeAnswers(answers);
+    this.setState({ randomAnswers });
+    this.countdown();
+  }
+
   createTestId = (answer, correctAnswer) => {
     const testId = answer === correctAnswer
       ? 'correct-answer' : `wrong-answer-${incorrectIdx}`;
@@ -11,16 +29,21 @@ export default class QuestionBoard extends Component {
     return testId;
   }
 
-  createButtons = (answers, correctAnswer) => {
+  randomizeAnswers = (answers) => {
     const randomNumber = 0.5;
+    return answers.sort(() => Math.random() - randomNumber);
+  }
+
+  createButtons = (answers, correctAnswer) => {
+    const { buttonState } = this.state;
     return (
       answers
-        .sort(() => Math.random() - randomNumber)
         .map((answer, idx) => (
           <button
             data-testid={ this.createTestId(answer, correctAnswer) }
             key={ idx }
             type="button"
+            disabled={ buttonState }
           >
             { answer }
           </button>
@@ -28,10 +51,30 @@ export default class QuestionBoard extends Component {
     );
   }
 
+  countdown = () => {
+    const oneSecondInMiliseconds = 1000;
+    const thirtySeconds = 30;
+    let count = thirtySeconds;
+    const interval = setInterval(() => {
+      count -= 1;
+      this.setState({ seconds: count }, () => {
+        const { seconds } = this.state;
+        if (seconds === 0) {
+          clearInterval(interval);
+          this.setState({ buttonState: true });
+        }
+      });
+    }, oneSecondInMiliseconds);
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.countdown);
+  }
+
   render() {
     const { questionInfo } = this.props;
     const { category, question, correct_answer: correctAnswer } = questionInfo;
-    const answers = [...questionInfo.incorrect_answers, correctAnswer];
+    const { seconds, randomAnswers } = this.state;
     return (
       <div>
         <h2
@@ -39,6 +82,7 @@ export default class QuestionBoard extends Component {
         >
           {category}
         </h2>
+        <p>{seconds}</p>
         <p
           data-testid="question-text"
         >
@@ -46,7 +90,7 @@ export default class QuestionBoard extends Component {
         </p>
         <div data-testid="answer-options">
           {
-            this.createButtons(answers, correctAnswer)
+            this.createButtons(randomAnswers, correctAnswer)
           }
         </div>
       </div>
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { MD5 } from 'crypto-js';
+import Header from '../components/Header';
 import QuestionBoard from '../components/QuestionBoard';
 import fetchQuestions from '../services/fetchApi';
 
@@ -11,13 +10,11 @@ class Game extends Component {
     this.state = {
       questions: undefined,
       questionIdx: 0,
-      srcImage: '',
     };
   }
 
   componentDidMount() {
     this.getQuestions();
-    this.setGravatarSrc();
   }
 
   getToken = () => localStorage.getItem('token');
@@ -38,31 +35,17 @@ class Game extends Component {
     }
   }
 
-  setGravatarSrc = () => {
-    const { state } = this.props;
-    const { player: { gravatarEmail } } = state;
-    const convertedEmail = MD5(gravatarEmail).toString();
-    const gravatarLink = `https://www.gravatar.com/avatar/${convertedEmail}`;
-    this.setState({ srcImage: gravatarLink });
-  }
-
   render() {
-    const { state } = this.props;
-    const { player: { name, score } } = state;
-    const { srcImage, questions, questionIdx } = this.state;
+    const { questions, questionIdx } = this.state;
     return (
       <div>
-        <header>
-          <img data-testid="header-profile-picture" alt="profile" src={ srcImage } />
-          <p data-testid="header-player-name">{name}</p>
-          <p data-testid="header-score">{score}</p>
-        </header>
-        <div>
+        <Header />
+        <main>
           Trivia
           {
             questions && <QuestionBoard questionInfo={ questions.results[questionIdx] } />
           }
-        </div>
+        </main>
       </div>
     );
   }
@@ -72,9 +55,6 @@ Game.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func,
   }).isRequired,
-  state: PropTypes.shape().isRequired,
 };
 
-const mapStateToProps = (state) => ({ state });
-
-export default connect(mapStateToProps)(Game);
+export default Game;
