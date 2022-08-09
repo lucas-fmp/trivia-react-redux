e8711995895c48da076ff0d101bc0aeb59398b57
lucas-fmp <lucasfernandomacedo13@gmail.com>
2022-08-09 15:13:01 -0300

    requirement 03 complete

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <Switch>
+      <Route component={ Login } exact path="/" />
       <Route component={ Game } path="/game" />
-      <Route component={ Login } path="/" />
+      <Route component={ Settings } path="/settings" />
     </Switch>
   );
 }
