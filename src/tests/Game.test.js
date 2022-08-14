40ee13ddc8f8ea992a49a53dd7ca85e8f76e039e
mateussousaa <mateussousa20@outlook.com>
2022-08-14 16:07:40 -0300

    feat: requirement 22 - completed

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { screen, waitFor } from '@testing-library/react';
+import { screen, waitForElementToBeRemoved } from '@testing-library/react';
 import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
 import userEvent from '@testing-library/user-event';
-import { questionsResponse, invalidTokenQuestionsResponse } from '../../cypress/mocks/questions';
+import { invalidTokenQuestionsResponse, questionsResponse } from '../../cypress/mocks/questions';
 import App from "../App";
 
 const MockedPlayer = {
@@ -16,6 +16,9 @@ const MockedPlayer = {
 const path = '/game';
 
 describe('The Game page', () => {
+  
+  afterEach(() => jest.clearAllMocks());
+
   it('contain the Header fields', () => {
     renderWithRouterAndRedux(<App />, MockedPlayer, path);
 
@@ -56,14 +59,44 @@ describe('The Game page', () => {
     expect(history.location.pathname).toBe('/feedback');
   });
 
-  it('redirect to the Login page if the fetch fail', async () => {
+  it('should test if the answers are disabled', async () => {
+    jest.useFakeTimers();
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve(questionsResponse),
+    }));
 
+    renderWithRouterAndRedux(<App />, MockedPlayer, path);
+    const correctAnswer = await screen.findByTestId('correct-answer');
+
+    jest.advanceTimersByTime(30000);
+
+    expect(correctAnswer).toHaveProperty('disabled', true);
+
+    jest.useRealTimers();
+  });
+
+  it('redirect to the Login page if the fetch fail', async () => {
     global.fetch = jest.fn(() => Promise.resolve({
       json: () => Promise.resolve(invalidTokenQuestionsResponse),
     }));
 
     const { history } = renderWithRouterAndRedux(<App />, MockedPlayer, path);
+    await waitForElementToBeRemoved(() => screen.getByTestId('header-score'))
 
-    expect(history.location.pathname).toBe('/game'); // porque nao só '/' ?
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('should test if the score is correct', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve({ ...questionsResponse, results: questionsResponse.results.filter(({ category }) => category !== "Geography")}),
+    }));
+
+    renderWithRouterAndRedux(<App />, MockedPlayer, path);
+    const correctAnswer = await screen.findByTestId('correct-answer');
+    userEvent.click(correctAnswer);
+    const buttonNext = screen.getByTestId('btn-next');
+    userEvent.click(buttonNext);
+    const score = screen.getByTestId("header-score");
+    expect(score).toHaveTextContent('100');
   });
 })
